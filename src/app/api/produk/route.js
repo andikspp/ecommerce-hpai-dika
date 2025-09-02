254b423af35e7887457ad41d2a3409020329fceb
andikspp <andhika2003.ap31@gmail.com>
2025-09-02 21:36:50 +0700

    tes fetch produk

--- src/app/api/produk/route.js
+++ src/app/api/produk/route.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 export async function GET(request) {
     try {
-        const res = await axios.get("http://localhost:5000/api/produk");
+        const apiUrl = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000";
+        const res = await axios.get(`${apiUrl}/api/produk`);
         return new Response(JSON.stringify(res.data), {
             status: res.status,
             headers: { "Content-Type": "application/json" },
